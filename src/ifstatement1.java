92c3cf29faa85cbff00834cd2d3d613ef125c8e3
S Masoom Basha <aahens.m143@gmail.com>
2022-03-02 15:07:58 +0530

    Initial commit

---
+++ src/ifstatement1.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+public class ifstatement1 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int a=sc.nextInt();
+        if(a>10){
+            System.out.println(true);
+        }
+        else{
+            System.out.println(false);
+        }
+    }
+}
